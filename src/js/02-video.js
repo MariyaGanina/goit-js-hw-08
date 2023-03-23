85942d25d50893c982f6c0b6062948574341efe1
MariyaGanina <122280294+MariyaGanina@users.noreply.github.com>
2023-03-23 19:17:30 +0100

    ex2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const updateTimeInStorage = throttle(time => {
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+player.on('timeupdate', data => {
+  const currentTime = data.seconds;
+  updateTimeInStorage(currentTime);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
