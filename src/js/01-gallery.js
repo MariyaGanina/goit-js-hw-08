5d89bb025ac524874b632114cafab1576b702a20
MariyaGanina <122280294+MariyaGanina@users.noreply.github.com>
2023-03-23 16:26:09 +0100

    ex1-done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const galleryTable = createGalleryTable(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryTable);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryTable(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+console.log(galleryItems);
