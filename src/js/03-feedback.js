7ffb395ecf4f67a1fc672647cfa6aace1308b1e1
MariyaGanina <122280294+MariyaGanina@users.noreply.github.com>
2023-03-23 21:12:51 +0100

    ex3-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+const updateFormState = throttle(() => {
+  const formState = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+const loadFormState = () => {
+  const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedState) {
+    emailInput.value = savedState.email;
+    messageInput.value = savedState.message;
+  }
+};
+
+form.addEventListener('input', updateFormState);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formState = { email: emailInput.value, message: messageInput.value };
+  console.log(formState);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
+
+loadFormState();
